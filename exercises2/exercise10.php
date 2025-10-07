cdc3e896f2369ad8f60a6327561be3329eb36a44
DavidHome <davidhlanz@gmail.com>
2025-10-07 21:20:31 +0200

    exercises 10,11,12 last ones

---
+++ exercises2/exercise10.php
@@ -0,0 +1,8 @@
+<?php
+    $begin = 0;
+
+    for($i = 1; $i<= 100; $i++){
+        $begin += $i;
+    }
+    echo $begin;
+?>  
---
+++ exercises2/exercise11.php
@@ -0,0 +1,8 @@
+<?php
+    $factorial = 1;
+
+    for($i = 1; $i<= 10; $i++){
+        $factorial *= $i;
+    }
+    echo $factorial;
+?>  
